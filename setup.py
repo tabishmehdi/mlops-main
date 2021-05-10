dbaba1868417ced4e6c2cd1d3179f5a1147598e6
Alina Zehra Mehdi <you@example.com>
2021-05-10 16:19:38 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='It is a Wafer Project with ML-OPS implementation',
+    author='Tabish Mehdi',
+    license='MIT',
+)
